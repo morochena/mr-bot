c81576f7c4936461cb82c00021ce616c6bf56692
Marcus Orochena <marcus.orochena@gmail.com>
2023-04-14 12:44:29 -0400

    refactor(bot.py): rename `client` variable to `bot` feat(bot.py): add logging and create Discord bot client using `interactions` library feat(bot.py): add support for Discord token environment variable refactor(bot.py): remove unused `discord` import refactor(bot.py): remove unused `load_dotenv()` call refactor(bot.py): remove unused `embedding_json` variable refactor(bot.py): remove unused `create_prompt()` function arguments refactor(bot.py): simplify `create_prompt()` function and improve readability refactor(bot.py): remove unused `print()` statement and replace with logging statement
    refactor(bot.py): reformat code to comply with PEP8 style guide
    feat(bot.py): add Discord bot commands for /question, /generate, and /help
    feat(bot.py): add logging to bot commands and error handling
    
    chore: update requirements.txt file

--- bot.py
+++ bot.py
@@ -1,22 +1,31 @@
+import interactions
 import sys
 import signal
 import os
+import logging
 
-import discord
 import openai
 import pandas as pd
 from dotenv import load_dotenv
 from openai.embeddings_utils import cosine_similarity, get_embedding
 
+
+# Load environment variables and API keys
 load_dotenv()
-
 openai.api_key = os.getenv('OPENAI_API_KEY')
+discord_token = os.getenv('DISCORD_TOKEN')
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Read embeddings JSON file
 embedding_json = pd.read_json('embeddings.json')
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
+# Create Discord bot client
+bot = interactions.Client(token=discord_token)
+
+# Signal handler for clean exit
 
 
 def signal_handler(signal, frame):
@@ -26,27 +35,42 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 
-def create_prompt(df, user_input, mode='summarize'):
-    if mode == 'summarize':
-        result, sources = search(df, user_input, n=3)
-        system_role = "You are a Discord chatbot whose expertise is reading and summarizing a roleplaying game rulebook called Mortal Reins. You are given a query and a series of text embeddings in order of their cosine similarity to the query. You must take the given embeddings and return a concise but accurate summary of the rules in the language of the query. Separate unrelated rules by bullet points if it helps make the rules more clear. Provide examples if possible."
-    else:
-        result, sources = search(df, user_input, n=3)
-        system_role = "You are a Discord chatbot whose expertise is reading a roleplaying game rulebook called Mortal Reins and creating generative content based on it. You are given a query and a series of text embeddings in order of their cosine similarity to the query. Generate creative content based on the given embeddings that correspond to the query, using the themes and information provided in the rulebook. Remember that Mortal Reins only uses d10 dice and that the game is set in a custom fantasy world."
+def create_prompt(df, user_input, mode='answer_question'):
+    result, sources = search(df, user_input, n=3)
 
-    user_content = """Here is the question: """ + user_input + """
-            
-Here are the embeddings: 
-            
-1.""" + str(result.iloc[0]['text']) + ("""
-2.""" + str(result.iloc[1]['text']) + """
-3.""" + str(result.iloc[2]['text']) + """\n""" if mode == 'summarize' else "\n")
+    if mode == 'answer_question':
+        system_role = ("You are a Discord chatbot whose expertise is reading and summarizing a roleplaying game "
+                       "rulebook called Mortal Reins. You are given a query and a series of text embeddings in order "
+                       "of their cosine similarity to the query. You must take the given embeddings and return a "
+                       "concise but accurate summary of the rules in the language of the query. Separate unrelated "
+                       "rules by bullet points if it helps make the rules more clear. Provide examples if possible.")
+
+        user_content = (f"Here is the question: {user_input}\n\n"
+                        f"Here are the embeddings:\n\n"
+                        f"1. {result.iloc[0]['text']}\n"
+                        f"2. {result.iloc[1]['text']}\n"
+                        f"3. {result.iloc[2]['text']}\n")
+
+    else:
+        system_role = ("You are a Discord chatbot whose expertise is reading a roleplaying game rulebook called "
+                       "Mortal Reins and creating generative content based on it. You are given a query and a series "
+                       "of text embeddings in order of their cosine similarity to the query. Generate creative "
+                       "content based on the given embeddings that correspond to the query, using the themes and "
+                       "information provided in the rulebook. Remember that Mortal Reins only uses d10 dice and "
+                       "that the game is set in a custom fantasy world.")
+
+        user_content = (f"Here is the base concept to generate from: {user_input}\n\n"
+                        f"Here are the embeddings:\n\n"
+                        f"1. {result.iloc[0]['text']}\n"
+                        f"2. {result.iloc[1]['text']}\n"
+                        f"3. {result.iloc[2]['text']}\n")
 
     messages = [
         {"role": "system", "content": system_role},
-        {"role": "user", "content": user_content},]
+        {"role": "user", "content": user_content},
+    ]
 
-    print('Done creating prompt')
+    logger.info('Done creating prompt')
     return messages, sources
 
 
@@ -58,143 +82,136 @@ def search(df, query, n=3):
     df["similarity"] = df.embeddings.apply(
         lambda x: cosine_similarity(x, query_embedding))
 
-    results = df.sort_values("similarity", ascending=False, ignore_index=True)
-    results = results.head(n)
+    results = df.sort_values(
+        "similarity", ascending=False, ignore_index=True).head(n)
 
-    sources = []
-    for i in range(n):
-        sources.append(
-            {'Page ' + str(results.iloc[i]['page']): results.iloc[i]['text'][:150] + '...'})
-    print(sources)
-    return results.head(n), sources
+    sources = [{'Page ' + str(results.iloc[i]['page']): results.iloc[i]
+                ['text'][:150] + '...'} for i in range(n)]
+    logger.info(sources)
+    return results, sources
 
 
 def gpt(messages, mode):
-    temperature = 0.7
-    if mode == 'generate':
-        temperature = 0.85
-    print('Sending request to GPT-3')
-    openai.api_key = os.getenv('OPENAI_API_KEY')
+    temperature = 0.85 if mode == 'generate' else 0.7
+    logger.info('Sending request to GPT-3')
+
     r = openai.ChatCompletion.create(
         model="gpt-3.5-turbo", messages=messages, temperature=temperature, max_tokens=1500)
+
     answer = r.choices[0]["message"]["content"]
-    print('Done sending request to GPT-3')
+    logger.info('Done sending request to GPT-3')
 
     response = {'answer': answer}
     return response
 
 
-def reply(df, question, mode='summarize'):
+def reply(df, question, mode='answer_question'):
     prompt, sources = create_prompt(df, question, mode)
     response = gpt(prompt, mode)
     response['sources'] = sources
     return response
 
 
-@client.event
-async def on_ready():
-    print('Logged in as {0.user}'.format(client))
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+@bot.command(name="question",
+             description="Ask a question related to the Mortal Reins rulebook.",
+             options=[
+                 interactions.Option(
+                     name="query",
+                     description="Your question about the rulebook.",
+                     type=interactions.OptionType.STRING,
+                     required=True
+                 )
+             ])
+async def question(ctx: interactions.CommandContext, query: str):
+    logger.info(f"Received question: {query}")
+    await ctx.defer()
+    try:
+        answer = reply(embedding_json, query)
+    except Exception as e:
+        logger.error(f"Error processing question: {str(e)}")
+        await ctx.send(f"Sorry, I couldn't find an answer to your question due to an error: {str(e)}")
         return
 
-    if message.content.startswith('!help'):
-        embed = discord.Embed(
-            title="Mortal Reins Discord Bot Help",
-            description="This bot specializes in reading and summarizing the Mortal Reins roleplaying game rulebook. It can also generate creative content based on the rulebook.",
-            color=0x741420
-        )
+    if 'answer' in answer:
+        logger.info("Sending response to question")
+        title = f"Answer to '{query}'"
+        if len(title) > 250:
+            title = title[:250] + "..."
+        embed = interactions.Embed(
+            title=title, description=answer['answer'], color=0x741420)
+
+        # Add sources to the embed
+        sources_text = ", ".join(
+            [f"{k}" for source in answer['sources'] for k, v in source.items()])
         embed.add_field(
-            name="!question [query]",
-            value="Ask the bot a question related to the Mortal Reins rulebook, and it will provide a summary of the most relevant rules.",
-            inline=False
-        )
-        embed.add_field(
-            name="!generate [query]",
-            value="Provide a theme or topic related to the Mortal Reins rulebook, and the bot will generate creative content based on the most similar rulebook page.",
-            inline=False
-        )
-        embed.add_field(
-            name="!help",
-            value="Shows this help message.",
-            inline=False
-        )
+            name="For more information, you can check the following pages in the rulebook:", value=sources_text, inline=False)
         embed.set_footer(
-            text="Please note that the answers provided may not be perfect. For the most accurate information, refer to the official rulebook.")
-        await message.channel.send(embed=embed)
+            text="Please note that the answer provided may not be perfect. For the most accurate information, refer to the official rulebook.")
 
-    if message.content.startswith('!question'):
-        query = message.content[10:]
-        try:
-            answer = reply(embedding_json, query)
-        except Exception as e:
-            await message.channel.send(f"Sorry, I couldn't find an answer to your question due to an error: {str(e)}")
-            return
-
-        if 'answer' in answer:
-            title = f"Answer to '{query}'"
-            if len(title) > 250:
-                title = title[:250] + "..."
-            embed = discord.Embed(
-                title=title, description=answer['answer'], color=0x741420)
-
-            # Add sources to the embed
-            sources_text = ", ".join(
-                [f"{k}" for source in answer['sources'] for k, v in source.items()])
-            embed.add_field(
-                name="For more information, you can check the following pages in the rulebook:", value=sources_text, inline=False)
-            embed.set_footer(
-                text="Please note that the answer provided may not be perfect. For the most accurate information, refer to the official rulebook.")
-
-            await message.channel.send(embed=embed)
-        else:
-            await message.channel.send("Sorry, I couldn't find an answer to your question.")
-
-    elif message.content.startswith('!generate'):
-        query = message.content[9:]
-        try:
-            answer = reply(embedding_json, query, mode='generate')
-        except Exception as e:
-            await message.channel.send(f"Sorry, I couldn't generate content based on your query due to an error: {str(e)}")
-            return
-
-        if 'answer' in answer:
-            title = f"Generated content for '{query}'"
-            if len(title) > 250:
-                title = title[:250] + "..."
-            embed = discord.Embed(
-                title=title, description=answer['answer'], color=0x741420)
-            await message.channel.send(embed=embed)
-        else:
-            await message.channel.send("Sorry, I couldn't generate content based on your query.")
+        await ctx.send(embeds=embed)
+    else:
+        await ctx.send("Sorry, I couldn't find an answer to your question.")
 
 
-client.run(os.getenv('DISCORD_TOKEN'))
+@bot.command(name="generate",
+             description="Generate creative content based on the Mortal Reins rulebook.",
+             options=[
+                 interactions.Option(
+                     name="query",
+                     description="A theme or topic related to the rulebook.",
+                     type=interactions.OptionType.STRING,
+                     required=True
+                 )
+             ])
+async def generate(ctx: interactions.CommandContext, query: str):
+    logger.info(f"Received generate request: {query}")
+    await ctx.defer()
+    try:
+        answer = reply(embedding_json, query, mode='generate')
+    except Exception as e:
+        logger.error(f"Error processing generate request: {str(e)}")
+        await ctx.send(f"Sorry, I couldn't generate content based on your query due to an error: {str(e)}")
+        return
 
-# interative cli if you pass in the --cli flag
-# otherwise run the bot
-# if __name__ == "__main__":
-#     import argparse
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('--cli', action='store_true')
-#     args = parser.parse_args()
+    if 'answer' in answer:
+        logger.info("Sending generated content")
+        title = f"Generated content for '{query}'"
+        if len(title) > 250:
+            title = title[:250] + "..."
+        embed = interactions.Embed(
+            title=title, description=answer['answer'], color=0x741420)
+        await ctx.send(embeds=embed)
+    else:
+        await ctx.send("Sorry, I couldn't generate content based on your query.")
 
-#     if args.cli:
-#         while True:
-#             query = input('Enter your question: ')
-#             try:
-#                 answer = reply(embedding_json, query)
-#             except Exception as e:
-#                 print(
-#                     f"Sorry, I couldn't find an answer to your question due to an error: {str(e)}")
-#                 continue
 
-#             if 'answer' in answer:
-#                 print(f"Answer to '{query}': {answer['answer']}")
-#             else:
-#                 print("Sorry, I couldn't find an answer to your question.")
-#     else:
-#         client.run(os.getenv('DISCORD_TOKEN'))
+@bot.command(name="help",
+             description="Show help information for the Mortal Reins bot.")
+async def help(ctx):
+    await ctx.defer()
+    embed = interactions.Embed(
+        title="Mortal Reins Discord Bot Help",
+        description="This bot specializes in reading and summarizing the Mortal Reins roleplaying game rulebook. It can also generate creative content based on the rulebook.",
+        color=0x741420
+    )
+    embed.add_field(
+        name="/question [query]",
+        value="Ask the bot a question related to the Mortal Reins rulebook, and it will provide a summary of the most relevant rules.",
+        inline=False
+    )
+    embed.add_field(
+        name="/generate [query]",
+        value="Provide a theme or topic related to the Mortal Reins rulebook, and the bot will generate creative content based on the most similar rulebook page.",
+        inline=False
+    )
+    embed.add_field(
+        name="/help",
+        value="Shows this help message.",
+        inline=False
+    )
+    embed.set_footer(
+        text="Please note that the answers provided may not be perfect. For the most accurate information, refer to the official rulebook.")
+    await ctx.send(embeds=embed)
+
+logger.info('Starting bot')
+bot.start()
